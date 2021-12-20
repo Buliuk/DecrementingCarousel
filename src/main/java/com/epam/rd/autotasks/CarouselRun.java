0527920b6fc7d977c508edd7b84b804bc24f5457
Evgenii_Efimchik <evgenii_efimchik@epam.com>
2021-12-20 21:30:04 +0300

    DecrementingCarousel has been done

--- src/main/java/com/epam/rd/autotasks/CarouselRun.java
+++ src/main/java/com/epam/rd/autotasks/CarouselRun.java
@@ -1,7 +1,8 @@
 package com.epam.rd.autotasks;
 
-public class CarouselRun {
+import java.util.Arrays;
 
+public class CarouselRun {
     public int next() {
        throw new UnsupportedOperationException();
     }
@@ -10,4 +11,17 @@ public class CarouselRun {
         throw new UnsupportedOperationException();
     }
 
+
+    public static void main(String[] args) {
+        DecrementingCarousel carousel = new DecrementingCarousel(7);
+        System.out.println(carousel.addElement(2)); //true
+        System.out.println(carousel.addElement(3)); //true
+        System.out.println(carousel.addElement(1)); //true
+
+        carousel.run();
+
+        System.out.println(carousel.addElement(2)); //false
+        System.out.println(carousel.addElement(3)); //false
+        System.out.println(carousel.addElement(1)); //false
+    }
 }
--- src/main/java/com/epam/rd/autotasks/DecrementingCarousel.java
+++ src/main/java/com/epam/rd/autotasks/DecrementingCarousel.java
@@ -1,15 +1,30 @@
 package com.epam.rd.autotasks;
 
 public class DecrementingCarousel {
-    public DecrementingCarousel(int capacity) {
+   private final int [] CAPACITY_ARR ;
+   private static int counter;
+   private static CarouselRun instance;
 
+    public DecrementingCarousel(int capacity) {
+        CAPACITY_ARR = new int[capacity];
     }
 
     public boolean addElement(int element){
-        throw new UnsupportedOperationException();
+        if (counter < getCAPACITY_ARR().length && element > 0 && instance == null){
+            CAPACITY_ARR [counter] = element;
+            counter++;
+            return true;
+        } else return false;
     }
 
     public CarouselRun run(){
-       throw new UnsupportedOperationException();
+        if (instance == null) {
+            instance = new CarouselRun();
+            return instance;
+        } else return null;
+    }
+
+    public int[] getCAPACITY_ARR() {
+        return CAPACITY_ARR;
     }
 }
